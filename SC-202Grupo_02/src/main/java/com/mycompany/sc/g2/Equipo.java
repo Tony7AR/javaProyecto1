1811dc46f2725ebd8a0a1e88be0e850056cb27d6
Oscarsolisb <osolis70261@ufide.ac.cr>
2024-10-28 01:03:13 -0600

    Machote

---
+++ SC-202Grupo_02/src/main/java/com/mycompany/sc/g2/Equipo.java
@@ -0,0 +1,79 @@
+package com.mycompany.sc.g2;
+/*
+CODIGO DE CLASE: SC-202
+GRUPO: 02
+INTEGRANTES: OSCAR SOLÍS BARRIENTOS, ANTHONY AZOFEIFA RAMÍREZ, ALESSANDRO BOGANTES CALERO
+|| PROYECTO FÚTBOL CINCO ||
+
+
+*/
+
+public class Equipo {
+    // || ATRIBUTOS ||
+    private static int idEquipo;
+    private String nombreEquipo;
+    private Jugador jugadores;
+    private static int partidosJugados;
+    private static int partidosGanados;
+    private static int partidosEmpatados;
+    private static int partidosPerdidos;
+    private static int golesFavor;
+    private static int golesContra;
+    private static int posicionBalon;
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    // || CONSTRUCTORES ||
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // || METODOS ||
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // || GETS AND SETS ||
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public Jugador getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(Jugador jugadores) {
+        this.jugadores = jugadores;
+    }
+    
+    
+    
+    
+}
---
+++ SC-202Grupo_02/src/main/java/com/mycompany/sc/g2/Jugador.java
@@ -0,0 +1,93 @@
+package com.mycompany.sc.g2;
+/*
+CODIGO DE CLASE: SC-202
+GRUPO: 02
+INTEGRANTES: OSCAR SOLÍS BARRIENTOS, ANTHONY AZOFEIFA RAMÍREZ, ALESSANDRO BOGANTES CALERO
+|| PROYECTO FÚTBOL CINCO ||
+
+
+*/
+
+public class Jugador {
+    // || ATRIBUTOS ||
+    private static int idJugador;
+    private String nombreJugador;
+    private Posicion posicion;
+    private Equipo equipoPertenencia;
+    private Estado estado;
+    private static int golesAnotados;
+    private static int accionesTotales;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // || CONSTRUCTORES ||
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // || METODOS ||
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // || GETS AND SETS ||
+
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public void setNombreJugador(String nombreJugador) {
+        this.nombreJugador = nombreJugador;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public Equipo getEquipoPertenencia() {
+        return equipoPertenencia;
+    }
+
+    public void setEquipoPertenencia(Equipo equipoPertenencia) {
+        this.equipoPertenencia = equipoPertenencia;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    
+   
+    
+    
+    
+}
